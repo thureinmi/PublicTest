5f6c09a032ce5bb7cd9eb077da303cf07277dbf2
thureinjat <thurein@akiyaresearch.com>
2023-07-20 10:49:50 +0630

    first commit

---
+++ MyToolBox/src/main/java/com/example/mytoolbox/Blink.kt
@@ -0,0 +1,31 @@
+package com.example.mytoolbox
+
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
+import android.graphics.Color
+import android.util.Log
+import android.view.View
+import android.view.animation.Animation
+
+object Blink {
+
+        fun blink(view: View) {
+            // adding the color to be shown
+            val animator: ObjectAnimator = ObjectAnimator.ofInt(
+                view, "backgroundColor", Color.YELLOW,
+                Color.RED, Color.GREEN
+            )
+            // duration of one color
+            animator.duration = 500;
+            animator.setEvaluator(ArgbEvaluator())
+            // color will be shown in reverse manner
+            animator.repeatCount = Animation.REVERSE
+            // Repeat up to infinite time
+            animator.repeatCount = Animation.INFINITE
+            animator.start()
+        }
+    fun tes(){
+        println("Hello Library!")
+    }
+
+}
